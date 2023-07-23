06932f11e0b49538d68afee3d6080deadcdf9fd0
mikko-o <40822203+mikko-o@users.noreply.github.com>
2023-07-23 14:47:38 +0300

    Edit db schema

--- src/db/indexer_schema.sql
+++ src/db/indexer_schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS last_indexed_blocks (
-  indexer_name VARCHAR (50) PRIMARY KEY,
+  indexer_name VARCHAR PRIMARY KEY,
   block BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS last_updated_at (
-  id VARCHAR (50) PRIMARY KEY,
+  id VARCHAR PRIMARY KEY,
   ts TIMESTAMP NOT NULL DEFAULT NOW()
 );
